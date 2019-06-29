a92afa364aa84671651faf6a50149b81dd104f00
Jesse Barkdoll <me@jessebarkdoll.com>
2019-06-29 12:18:37 -0400

    set up project with first completed

---
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -0,0 +1,14 @@
+function absoluteValuesSumMinimization(a: number[]): number {
+    const isEven = a.length % 2 === 0;
+    
+    const targetIndex = isEven 
+        ? a.length / 2 - 1
+        : Math.floor(a.length / 2);
+
+    return a[targetIndex];
+}
+
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
