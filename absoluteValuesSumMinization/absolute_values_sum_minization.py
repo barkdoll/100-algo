3a6c083d95e061ff58d294e23ce730f3cdc79082
Jesse Barkdoll <me@jessebarkdoll.com>
2019-06-29 14:41:52 -0400

    snake-case challenge solution fn because pep8

--- absoluteValuesSumMinization/absolute_values_sum_minization.py
+++ absoluteValuesSumMinization/absolute_values_sum_minization.py
@@ -3,13 +3,13 @@ from typing import List
 import math
 
 
-def absoluteValuesSumMinimization(a: List[int]) -> int:
+def abs_vals_sum_minization(a: List[int]) -> int:
     is_even = len(a) % 2 == 0
     target_index = len(a) / 2 - 1 if is_even else math.floor(len(a) / 2) 
     return a[int(target_index)]
 
 
-print(absoluteValuesSumMinimization([2, 4, 7]))
-print(absoluteValuesSumMinimization([2, 4, 7, 6]))
-print(absoluteValuesSumMinimization([2, 4, 7, 6, 6]))
-print(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]))
+print(abs_vals_sum_minization([2, 4, 7]))
+print(abs_vals_sum_minization([2, 4, 7, 6]))
+print(abs_vals_sum_minization([2, 4, 7, 6, 6]))
+print(abs_vals_sum_minization([2, 4, 7, 6, 6, 8]))
