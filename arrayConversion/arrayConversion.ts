ce78688a11d3a09f63711cd2b4aeea2bf37a2a3a
Jesse <jessebarkdoll@gmail.com>
2019-07-03 21:16:51 -0400

    make variable names consistent for both versions

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,21 +1,21 @@
 const arrayConversion = (inputArray: number[]): number => {
 	const sum = (a, b): number => a + b;
 	const product = (a, b): number => a * b;
-	const alternate = (input: number[], iteration: number = 0) => {
-		if ( input.length === 1 )
+	const alternate = (bundle: number[], iteration: number = 0) => {
+		if ( bundle.length === 1 )
 		{
-			return input.pop();
+			return bundle.pop();
 		}
 
 		const processed = iteration % 2 === 0
-			? input.reduce( (gatherer, x, i) => i % 2 === 0 
-				? [...gatherer, sum(x, input[i+1]) ] 
+			? bundle.reduce( (gatherer, x, i) => i % 2 === 0
+				? [...gatherer, sum(x, bundle[i+1]) ]
 				: gatherer, [] )
-			: input.reduce( (gatherer, x, i) => i % 2 === 0 
-				? [...gatherer, product(x, input[i+1]) ] 
+			: bundle.reduce( (gatherer, x, i) => i % 2 === 0
+				? [...gatherer, product(x, bundle[i+1]) ]
 				: gatherer, [] )
 
-		return alternate(processed, iteration+1);  
+		return alternate(processed, iteration+1);
 	}
 
 	return alternate(inputArray);
@@ -24,7 +24,7 @@ const arrayConversion = (inputArray: number[]): number => {
 
 const inputs: number[][] = [
 	[1, 2, 3, 4, 5, 6, 7, 8],
-	[2, 5, 9, 4, 1, 6, 5, 2]	
+	[2, 5, 9, 4, 1, 6, 5, 2]
 ];
 
 console.time('arrayConversion x2');
